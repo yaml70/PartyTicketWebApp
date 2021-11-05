c5791cc0c1f3192a5f42fb679876f49845087de2
yaml70 <68991735+yaml70@users.noreply.github.com>
2021-11-05 19:32:19 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace partywebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
