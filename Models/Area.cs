29750d8450a419f2e66878ff70b00da0cd94f5c1
yaml70 <68991735+yaml70@users.noreply.github.com>
2021-11-05 19:48:44 +0200

    adding area model

---
+++ Models/Area.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace partywebapp.Models
+{
+    public enum AreaType
+    {
+        [Display(Name = "Center")]
+        Center = 1,
+        [Display(Name = "North")]
+        North = 2,
+        [Display(Name = "South")]
+        South = 3,
+        [Display(Name = "Hashron")]
+        Hasharon = 4,
+    }
+    public class Area
+    {
+        public int Id { get; set; }
+        public AreaType Type { get; set; }
+        public List<Party> Parties { get; set; }
+    }
+}
