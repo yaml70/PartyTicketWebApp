c5bbe60a8dbf596e9cb6b5d40d038fc0875eab16
yaml70 <68991735+yaml70@users.noreply.github.com>
2021-11-05 20:08:26 +0200

    creating web party context and db context

--- Data/PartyWebAppContext.cs
+++ Data/PartyWebAppContext.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace partywebapp.Data
 {
-    public class PartyWebAppContext
+    public class PartyWebAppContext : DbContext
     {
+        public PartyWebAppContext(DbContextOptions<PartyWebAppContext> options)
+: base(options)
+        {
+        }
+        public DbSet<partywebapp.Models.Party> Party { get; set; }
+        public DbSet<partywebapp.Models.Performer> Performer { get; set; }
+        public DbSet<partywebapp.Models.PartyImage> PartyImage { get; set; }
+        public DbSet<partywebapp.Models.User> User { get; set; }
+        public DbSet<partywebapp.Models.Area> Area { get; set; }
+        public DbSet<partywebapp.Models.Genre> Genre { get; set; }
+        public DbSet<partywebapp.Models.Club> Club { get; set; }
     }
 }
