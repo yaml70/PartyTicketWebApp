e4390b590ba0e82a9949e157ab554152b8d6b242
yaml70 <68991735+yaml70@users.noreply.github.com>
2021-11-05 19:54:51 +0200

    creating genre model

---
+++ Models/Genre.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace partywebapp.Models
+{
+
+    public enum GenreType
+    {
+        None = 0x0,
+        [Display(Name = "Hip Hop")]
+        HipHop = 1,
+        [Display(Name = "Rock")]
+        Rock = 2,
+        [Display(Name = "Techno")]
+        Techno = 3,
+        [Display(Name = "House")]
+        House = 4,
+        [Display(Name = "Pop")]
+        Pop = 5,
+    }
+
+    public class Genre
+    {
+        public int Id { get; set; }
+        public GenreType Type { get; set; }
+        public List<Party> parties { get; set; }
+    }
+}
