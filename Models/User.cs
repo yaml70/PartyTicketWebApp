3baa44cb6d24432bd04f3c1294bbb38e37455779
yaml70 <68991735+yaml70@users.noreply.github.com>
2021-11-05 20:05:17 +0200

    creating user model

---
+++ Models/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace partywebapp.Models
+{
+
+    public enum UserType
+    {
+        Client,
+        producer,
+        Admin
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string password { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string email { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        [Display(Name = "Birth Date")]
+        public DateTime birthDate { get; set; }
+        public UserType Type { get; set; } = UserType.Client;
+        public List<Party> parties { get; set; }
+    }
+}
