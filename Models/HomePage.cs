32ace79b638645d867595dec12504ff2bb732da4
yaml70 <68991735+yaml70@users.noreply.github.com>
2021-11-05 19:56:25 +0200

    creating homepage model

---
+++ Models/HomePage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace partywebapp.Models
+{
+    public class HomePage
+    {
+        public List<Club> clubs { get; set; }
+        public List<Performer> performers { get; set; }
+        public List<Party> parties { get; set; }
+    }
+}
