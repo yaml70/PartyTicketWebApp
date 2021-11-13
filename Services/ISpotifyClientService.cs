8a76803aa32c41fb07233107ee12410775d9f8e3
yaml70 <68991735+yaml70@users.noreply.github.com>
2021-11-13 14:21:03 +0200

    delete comments

--- Services/ISpotifyClientService.cs
+++ Services/ISpotifyClientService.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-//using SpotifyAPI.Web;
+using SpotifyAPI.Web;
 
 
 namespace partywebapp.Services
 {
     public interface ISpotifyClientService
     {
-        /*
+        
         Task<List<FullArtist>> GetArtists(List<string> artists);
 
         Task<FullArtist> GetArtist(string artistId);
 
         Task<string> getArtistIdBySearchParams(string searchParams);
-        */
+        
     }
 }
